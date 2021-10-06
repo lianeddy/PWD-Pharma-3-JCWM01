b67236adebf598e2651a7d760309e04a73d0b427
pramputra <prambann@gmail.com>
2021-10-06 16:14:30 +0700

    Login and Register UI
    adding the design of the login and register page with new icon and logo.

--- folder-fe/src/pages/login/loginPage.js
+++ folder-fe/src/pages/login/loginPage.js
@@ -1,118 +1,154 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
 
-import React, { useState } from 'react';
-import Axios from 'axios';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import { makeStyles } from "@material-ui/core/styles";
 
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 
-
-
-
+import Logo from "../../assets/img/logo/Klinik.png";
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    background: "#03989e",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   paper: {
-    marginTop: theme.spacing(22),
+    padding: theme.spacing(3),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+  title: {
+    color: "#03989e",
+    fontSize: 24,
+  },
+  subtitle: {
+    color: "#6fbc97",
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: "100%",
     marginTop: theme.spacing(1),
   },
   submit: {
+    borderRadius: 8,
+    backgroundColor: "#03989e",
     margin: theme.spacing(3, 0, 2),
+    "&:hover": {
+      backgroundColor: "#03989e",
+    },
   },
 }));
 
-export default function SignIn() {
+export default function Signin() {
   const classes = useStyles();
+  const history = useHistory();
 
-  const  [username, setUsername] = useState("");
-  const  [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  // create function to handle button login
+  const handleSignin = () => {
+    axios
+      .post("http://localhost:3300/users/login", {
+        username: username,
+        password: password,
+      })
+      .then((res) => {
+        // kalo sukses redirect ke home
+      })
+      .catch((err) => {
+        // kalo error ngapain?
+      });
+  };
 
-  
+  const goToSignup = () => {
+    history.push("/register");
+
+  }
+
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <div className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-            onChange={(event)=>{
-                setUsername(event.target.value)
-            }}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={(event)=>{
-                setPassword(event.target.value)
-            }}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="secondary"
-            className={classes.submit}
-            onClick={()=>{
-                console.log("test", username, password)
-                Axios.post('http://localhost:3300/users/login', {
-                    username: username,
-                    password: password
-                }).then((res) => {
-                    console.log("sukses")
-                }).catch((err) => {console.log(err)})  
-            }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </div>
-      </div>
-    </Container>
+    <div className={classes.container}>
+      <Container component="main" maxWidth="xs">
+        <Paper elevation={3} className={classes.paper}>
+          {/* START OF LOGO SECTION */}
+          <Box p={2} textAlign="center">
+            <img src={Logo} width="125px" alt="logo" />
+            <Box mt={1}>
+              <Typography className={classes.title}>
+                <b>KLINIK-KU</b>
+              </Typography>
+              <Typography className={classes.subtitle}>
+                APOTEK ONLINE
+              </Typography>
+            </Box>
+          </Box>
+          {/* END OF LOGO SECTION */}
+
+          <div className={classes.form} noValidate>
+            {/* USERNAME */}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+            />
+
+            {/* PASSWORD */}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <Button
+              fullWidth
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleSignin}
+            >
+              Masuk
+            </Button>
+            <div>
+              <Typography variant="body2">
+                Belum punya akun?{" "}
+                <Link onClick={goToSignup} variant="body2">
+                  Daftar di sini.
+                </Link>
+              </Typography>
+            </div>
+          </div>
+        </Paper>
+      </Container>
+    </div>
   );
-}
+};
+
+
--- folder-fe/src/pages/register/registerPage.js
+++ folder-fe/src/pages/register/registerPage.js
@@ -1,145 +1,180 @@
-import React, { useState } from 'react';
-import Axios from 'axios';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
 
 
+import { makeStyles } from "@material-ui/core/styles";
+
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+
+import Logo from "../../assets/img/logo/Klinik.png";
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(22),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+  wrapper: {
+    background: "#03989e",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+  paper: {
+    padding: theme.spacing(3),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  title: {
+    color: "#03989e",
+    fontSize: 24,
+  },
+  subtitle: {
+    color: "#6fbc97",
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
+    width: "100%",
+    marginTop: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    borderRadius: 8,
+    backgroundColor: "#03989e",
+    margin: theme.spacing(2, 0, 2),
+    "&:hover": {
+      backgroundColor: "#03989e",
+    },
   },
 }));
 
 export default function RegisterPage() {
   const classes = useStyles();
+  const history = useHistory();
 
   const [full_name, setFullName] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  // create function to handle button login
+  const handleSignup = () => {
+    axios
+      .post("http://localhost:3300/users/register", {
+        full_name: full_name,
+        username: username,
+        password: password,
+        email: email,
+      })
+      .then((res) => {
+        console.log("sukses");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const goToSignin = () => {
+    history.push("/login");
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
+    <div className={classes.wrapper}>
+      <Container component="main" maxWidth="xs">
+        <Paper elevation={3} className={classes.paper}>
+          {/* START OF LOGO SECTION */}
+          <Box p={2} textAlign="center">
+            <img src={Logo} width="96px" alt="logo" />
+            <Box mt={1}>
+              <Typography className={classes.title}>
+                <b>KLINIK-KU</b>
+              </Typography>
+              <Typography className={classes.subtitle}>
+                APOTEK ONLINE
+              </Typography>
+            </Box>
+          </Box>
+          {/* END OF LOGO SECTION */}
 
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              autoComplete="fname"
-              name="full_name"
-              variant="outlined"
-              required
-              fullWidth
-              id="full_name"
-              label="Full Name"
-              autoFocus
-              onChange={(event) => {
-                setFullName(event.target.value)
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              autoComplete="uname"
-              name="username"
-              variant="outlined"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              autoFocus
-              onChange={(event) => {
-                setUsername(event.target.value)
-              }}
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              onChange={(event) => {
-                setEmail(event.target.value)
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(event) => {
-                setPassword(event.target.value)
-              }}
-            />
-          </Grid>
-        </Grid>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="secondary"
-          className={classes.submit}
-          onClick={() => {
-            if (username === "" || email === "" || password === "") {
-              alert('Fill in all the form')
-            } else {
-              Axios.post('http://localhost:3300/users/register', {
-                full_name: full_name,
-                username: username,
-                password: password,
-                email: email
-              }).then((res) => {
-                console.log("sukses")
-              }).catch((err) => { console.log(err) })
-            }
-            console.log("test", username, password, full_name, email)
-
-          }}
-        >
-          Sign Up
-        </Button>
-        <Grid container justifyContent="flex-end">
-          <Grid item>
-            <Link href="#" variant="body2">
-              Already have an account? Sign in
-            </Link>
-          </Grid>
-        </Grid>
-
-      </div>
-    </Container>
+          <TextField
+            margin="normal"
+            autoComplete="fname"
+            name="full_name"
+            variant="outlined"
+            required
+            fullWidth
+            id="full_name"
+            label="Full Name"
+            autoFocus
+            onChange={(event) => {
+              setFullName(event.target.value);
+            }}
+          />
+          <TextField
+            margin="normal"
+            autoComplete="uname"
+            name="username"
+            variant="outlined"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            autoFocus
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <TextField
+            margin="normal"
+            variant="outlined"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <TextField
+            margin="normal"
+            variant="outlined"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+            onClick={handleSignup}
+          >
+            Daftar
+          </Button>
+          <div>
+            <Typography variant="body2">
+              Sudah punya akun?{" "}
+              <Link onClick={goToSignin} variant="body2">
+                Masuk di sini.
+              </Link>
+            </Typography>
+          </div>
+        </Paper>
+      </Container>
+    </div>
   );
 }
+
+
