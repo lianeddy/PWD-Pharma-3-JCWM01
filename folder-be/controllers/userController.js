3b67b4a830ab1e8a23a0854bd89067540099dfbe
taksu01 <taksuwijaya07@gmail.com>
2021-10-06 08:50:28 +0700

    verify send and  folder restructure

--- folder-be/controllers/userController.js
+++ folder-be/controllers/userController.js
@@ -1,15 +1,27 @@
 const { db } = require("../database");
 const Crypto = require("crypto");
+const { sendVerify } = require("./verification/verificationController");
 
 module.exports = {
-    getData: (req, res) => {
-        let scriptQuery = `Select * from users where username=${db.escape(req.body.username)} and password=${db.escape(req.body.password)};`
-        console.log(req.body, scriptQuery)
-        db.query(scriptQuery, (err, results) => {
-            if (err) res.status(500).send(err)
-            if (results[0]) {
-                res.status(200).send({dataLogin: results[0], message: "Login Success" })
-            }
-        })
+  getData: (req, res) => {
+    let scriptQuery = `Select * from users where username=${db.escape(
+      req.body.username
+    )} and password=${db.escape(req.body.password)};`;
+    console.log(req.body, scriptQuery);
+    db.query(scriptQuery, (err, results) => {
+      if (err) res.status(500).send(err);
+      if (results[0]) {
+        res
+          .status(200)
+          .send({ dataLogin: results[0], message: "Login Success" });
+      }
+    });
+  },
+  testMail: (req, res) => {
+    let verify = sendVerify(req.query.email, "lalalalala");
+    if (verify) {
+      res.status(500).send({ message: "Fail", success: false });
     }
-}
+    res.status(200).send({ message: "Success", success: true });
+  },
+};
--- folder-be/routers/userRouters.js
+++ folder-be/routers/userRouters.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { userControllers } = require("../controllers");
 const routers = express.Router();
 
-routers.post('login', userControllers.getData)
+routers.post("login", userControllers.getData);
+routers.post("/testVerify", userControllers.testMail);
 
 module.exports = routers;
