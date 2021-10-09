337746cc64d5ae00d20509b9f0e4f83ac6f63d8c
pramputra <prambann@gmail.com>
2021-10-09 17:05:12 +0700

    [PP3J-10-Authorization/Authentication] create limit acces, for non admin cannot acces admin dashboard

---
+++ folder-fe/src/pages/tempLanding/TemptLanding.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+const TemptLanding = (props) => {
+    return(
+        <div>
+            <h1>ini landing page</h1>
+        </div>
+    )
+
+}
+
+export default TemptLanding;
